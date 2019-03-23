ded31477ce19910fc40480654c8d206121dca68d
LHRobs <roberto_masocco@hotmail.com>
2019-03-24 00:05:08 +0100

    Tidying up...

---
+++ BCI_SamplingTest/cmake-build-debug/CMakeFiles/BCI_SamplingTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BCI_SamplingTest.dir/main.c.o"
+  "BCI_SamplingTest.pdb"
+  "BCI_SamplingTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BCI_SamplingTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BCI_SamplingTest/cmake-build-debug/CMakeFiles/BCI_SamplingTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BCI_SamplingTest.
+# This may be replaced when dependencies are built.
