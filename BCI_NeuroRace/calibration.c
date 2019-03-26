5e70460f05047ab676c44e0222babc0b7d93b775
LHRobs <roberto_masocco@hotmail.com>
2019-03-26 22:07:10 +0100

    Tidied up routine files.

--- BCI_NeuroRace/calibration.c
+++ BCI_NeuroRace/calibration.c
@@ -1,10 +1,16 @@
+// Includete quello che vi serve.
+
 #include "BCI_NeuroRace.h"
+
+// Mettete come dati globali quello che vi serve tirar fuori
+// come risultato della calibrazione e referenziateli in control.c
+// mettendo prima "extern", come sotto.
 
 /* Data acquired for a calibration run. */
 extern double calibrationData[CHANNELS][CALIBRATION_SECONDS][BINS];
 // Prima dimensione: canali, 2.
 // Seconda dimensione: secondi di acquisizione, 5.
-// Terza dimensione: frequenze d'interesse, 6 (8, 10, 11, 13, 22, 26 ordinate
+// Terza dimensione: frequenze d'interesse, 6 (8, 10, 11, 13, 22, 26 Hz ordinate
 // così).
 
-// Calibrazione divisa in fasi, una funzione per fase (?)
+// Calibrazione divisa in fasi, una funzione per fase.
--- BCI_NeuroRace/control.c
+++ BCI_NeuroRace/control.c
@@ -1,7 +1,10 @@
+// Includete quello che vi serve.
+
 #include "BCI_NeuroRace.h"
 
-// Array ordinato per canali e frequenze. Contiene le ampiezze a 1 sec.
-// Le frequenze sono rispettivamente 8, 10, 11, 13, 22 e 26 Hz.
+// Array ordinato per canali e frequenze rispettivamente.
+// Contiene le ampiezze a 1 sec.
+// Le frequenze sono rispettivamente 8, 10, 11, 13, 22 e 26 Hz (BINS = 6).
 extern double amplitudes[CHANNELS][BINS];
 
 // Unica funzione che restituisce -1 (sx), 0 (centro) o 1 (dx).
