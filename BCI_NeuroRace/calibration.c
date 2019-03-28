30bdf7316fb1169aaaa4d9b167ac63ae83dc8508
francescacl <fracl.carpineta@gmail.com>
2019-03-28 10:21:37 +0100

    Calibrazione

--- BCI_NeuroRace/calibration.c
+++ BCI_NeuroRace/calibration.c
@@ -1,10 +1,13 @@
-// Includete quello che vi serve.
-
+#include <math.h>
 #include "BCI_NeuroRace.h"
 
-// Mettete come dati globali quello che vi serve tirar fuori
-// come risultato della calibrazione e referenziateli in control.c
-// mettendo prima "extern", come sotto.
+double lim_eight = HUGE_VAL;
+double lim_ten = HUGE_VAL;
+double lim_eleven = HUGE_VAL;
+double lim_thirteen = HUGE_VAL;
+double max_eleven = 0;
+double max_thirteen = 0;
+
 
 /* Data acquired for a calibration run. */
 extern double calibrationData[CHANNELS][CALIBRATION_SECONDS][BINS];
@@ -13,4 +16,53 @@ extern double calibrationData[CHANNELS][CALIBRATION_SECONDS][BINS];
 // Terza dimensione: frequenze d'interesse, 6 (8, 10, 11, 13, 22, 26 Hz ordinate
 // così).
 
-// Calibrazione divisa in fasi, una funzione per fase.
+
+void min(double A[][][], int j) {
+	double m;
+	for(int i = 0, i < 2, i++) {
+		for(int t = 0; t < 5; t++) {
+			if(A[i][t][j] < m)
+				m = A[i][t][j];
+		}
+	}
+	return m;
+}
+
+
+void max(double A[][][], int j) {
+	double M;
+	for(int i = 0, i < 2, i++) {
+		for(int t = 0; t < 5; t++) {
+			if(A[i][t][j] > M)
+				M = A[i][t][j];
+		}
+	}
+	return M;
+}
+
+
+void closedEyes() {
+	lim_eight = min(calibrationData, 0);
+	lim_ten = min(calibrationData, 1);
+}
+
+
+void ledSx() {
+// 11 Hz
+	lim_eleven = min(calibrationData, 2);
+}
+
+
+void ledDx() {
+// 13 Hz
+	lim_thirteen = min(calibrationData, 3);
+}
+
+
+void lookAtScreen() {
+	max_eleven = max(calibrationData, 2);
+	max_thirteen = max(calibrationData, 3);
+
+	lim_eleven = (lim_eleven + max_eleven)/2.0;
+	lim_thirteen = (lim_thirteen + max_thirteen)/2.0;
+}
--- BCI_NeuroRace/control.c
+++ BCI_NeuroRace/control.c
@@ -7,4 +7,11 @@
 // Le frequenze sono rispettivamente 8, 10, 11, 13, 22 e 26 Hz (BINS = 6).
 extern double amplitudes[CHANNELS][BINS];
 
+extern double lim_eight;
+extern double lim_ten;
+extern double lim_eleven;
+extern double lim_thirteen;
+
 // Unica funzione che restituisce -1 (sx), 0 (centro) o 1 (dx).
+
+
