9bf75a3bb05b69d9326003e46ccc9edfc3f9965e
LHRobs <roberto_masocco@hotmail.com>
2019-03-28 22:19:15 +0100

    Added a tester, fixed a bug.

--- BCI_NeuroRace/Testers/socketLerci.c
+++ BCI_NeuroRace/Testers/socketLerci.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <signal.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 
-#include "BCI_NeuroRace.h"
-#include "CommProtocol.h"
-
-#define PI_IP "192.168.2.143"
+#define PI_IP "192.168.2.111"
 #define GAME_PORT 4444
 #define BACKLOG 1
 
-extern pid_t procPID;
-
-extern sigset_t workerSet;
-
-extern __thread void (*closeRoutine)(void);
-
-extern sem_t dataLocks[CHANNELS][2];
-extern sem_t startSampling, startCalibration, endCalibration;
-
-extern pthread_mutex_t controlLock;
+/* New shit. */
+#define CALIBRATION_DURATION 5
+#define CONTROL_DURATION 1
 
 /* Socket data. */
 int sock, gameSock;
@@ -36,22 +25,26 @@ struct sockaddr_in sockAddr, gameAddr;
 socklen_t gameAddrLen;
 
 /* Function to determine input for the game. */
-int control(void);
+int control(void) {
+    static int firstTime = 1;
+    if (firstTime) {
+        firstTime = 0;
+        srand(time(NULL));
+    }
+    int comms[] = {-1, 0, 1};
+    return comms[rand() % 3];
+}
 
-/* Data of thread spawned by controller thread. */
-pthread_t calibratorTID;
-extern pthread_attr_t calibratorData;
-void *calibrator(void *arg);
+void terminator(int sig) {
+    (void)sig;
+    printf("Leeezzo!\n");
+    exit(EXIT_SUCCESS);
+}
 
-/* Functions in this file. */
-void *controller(void *arg);
-void controllerTermination(void);
-
-/* Thread that processes signals and generates game input. */
-void *controller(void *arg) {
-    UNUSED(arg);
-    pthread_sigmask(SIG_SETMASK, &workerSet, NULL);
-    closeRoutine = controllerTermination;
+/* Shitty copypaste of one and a half threads. */
+int main(void) {
+    signal(SIGINT, terminator);
+    signal(SIGTERM, terminator);
     int controlRes;
     char msg;
     ssize_t recvRes, sendRes;
@@ -60,12 +53,12 @@ void *controller(void *arg) {
     if (sock < 0) {
         fprintf(stderr, "ERROR: Failed to open socket.\n");
         perror("socket");
-        kill(procPID, SIGTERM);
+        exit(EXIT_FAILURE);
     }
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes)) {
         fprintf(stderr, "ERROR: Failed to configure socket.\n");
         perror("setsockopt");
-        kill(procPID, SIGTERM);
+        exit(EXIT_FAILURE);
     }
     memset(&sockAddr, 0, sizeof sockAddr);
     memset(&gameAddr, 0, sizeof gameAddr);
@@ -75,12 +68,12 @@ void *controller(void *arg) {
     if (bind(sock, (struct sockaddr *)&sockAddr, sizeof sockAddr)) {
         fprintf(stderr, "ERROR: Failed to bind socket to Ethernet port.\n");
         perror("bind");
-        kill(procPID, SIGTERM);
+        exit(EXIT_FAILURE);
     }
     if (listen(sock, BACKLOG)) {
         fprintf(stderr, "ERROR: Failed to set socket as listening.\n");
         perror("listen");
-        kill(procPID, SIGTERM);
+        exit(EXIT_FAILURE);
     }
     printf("Socket opened.\n");
     // Wait for game to connect.
@@ -88,7 +81,7 @@ void *controller(void *arg) {
     if (gameSock < 0) {
         fprintf(stderr, "ERROR: Failed to accept connection with game.\n");
         perror("accept");
-        kill(procPID, SIGTERM);
+        exit(EXIT_FAILURE);
     }
     close(sock);
     printf("Connected with game.\n");
@@ -97,59 +90,70 @@ void *controller(void *arg) {
         recvRes = recv(gameSock, &msg, 1, 0);
         if (recvRes == 0) {
             // Socket closed: terminate process.
-            kill(procPID, SIGTERM);
+            exit(EXIT_FAILURE);
         } else if (recvRes == -1) {
             fprintf(stderr,
                     "ERROR: Failed to receive message on socket.\n");
             perror("recv");
-            kill(procPID, SIGTERM);
+            exit(EXIT_FAILURE);
         }
-    } while (msg != START_CALIBRATION);
-    // Spawn calibrator thread.
-    if (pthread_create(&calibratorTID, &calibratorData, calibrator, NULL)) {
-        fprintf(stderr, "ERROR: Failed to spawn calibrator thread.\n");
-        close(gameSock);
-        kill(procPID, SIGTERM);
+    } while (msg != 'S');
+
+    // NOW THE REAL COPYPASTE SHIT STARTS //
+
+    msg = 'E';
+    printf("First calibration started.\n");
+    // Simulate filter startup.
+    sleep(2);
+    sendRes = send(gameSock, &msg, 1, MSG_NOSIGNAL);
+    if (sendRes != 1) {
+        fprintf(stderr, "ERROR: Failed to send message on socket.\n");
+        perror("send");
+        exit(EXIT_FAILURE);
     }
-    // Start sampling.
-    sem_post(&startSampling);
-    // Start calibration.
-    sem_post(&startCalibration);
-    // Wait for first calibration to end.
-    sem_wait(&endCalibration);
-    // Control loop.
+    printf("Filters setup completed.\n");
+    // Simulate calibration steps.
+    for (int i = 0; i < 7; i++) {
+        // Wait for game calibration start message.
+        do {
+            recvRes = recv(gameSock, &msg, 1, 0);
+            if (recvRes == 0) {
+                // Socket closed: terminate process.
+                exit(EXIT_FAILURE);
+            } else if (recvRes == -1) {
+                fprintf(stderr,
+                        "ERROR: Failed to receive message on socket.\n");
+                perror("recv");
+                exit(EXIT_FAILURE);
+            }
+        } while (msg != 'S');
+        sleep(CALIBRATION_DURATION);
+        sendRes = send(gameSock, &msg, 1, MSG_NOSIGNAL);
+        if (sendRes != 1) {
+            fprintf(stderr, "ERROR: Failed to send message on socket.\n");
+            perror("send");
+            exit(EXIT_FAILURE);
+        }
+        printf("Calibration phase %d done.\n", i + 1);
+    }
+    printf("Initial calibration completed.\n");
+    // Simulate control.
     for (;;) {
-        // Acquire data processing right.
-        pthread_mutex_lock(&controlLock);
-        // Wait for data to be ready.
-        for (int i = 0; i < CHANNELS; i++)
-            sem_wait(&(dataLocks[i][1]));
-        // Compute control.
+        sleep(CONTROL_DURATION);
         controlRes = control();
         if (controlRes == -1) {
-            msg = LEFT;
+            msg = 'L';
         } else if (controlRes == 1) {
-            msg = RIGHT;
-        } else msg = CENTER;
-        // Release data channels.
-        for (int i = 0; i < CHANNELS; i++)
-            sem_post(&(dataLocks[i][0]));
-        // Release data processing right.
-        pthread_mutex_unlock(&controlLock);
+            msg = 'R';
+        } else msg = 'C';
         // Send control message.
         sendRes = send(gameSock, &msg, 1, MSG_NOSIGNAL);
         if (sendRes != 1) {
             fprintf(stderr, "ERROR: Failed to send message on socket.\n");
             perror("send");
-            kill(procPID, SIGTERM);
+            exit(EXIT_FAILURE);
         }
         printf("Sent control: '%c'.\n", msg);
     }
-}
-
-/* Termination procedure for controller thread. */
-void controllerTermination(void) {
-    pthread_kill(calibratorTID, SIGUSR1);
-    pthread_join(calibratorTID, NULL);
-    pthread_exit(NULL);
+    return 0;
 }
